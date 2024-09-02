19e7934240ca27f5a7f181d154f8a6d3c6c097d9
nisha <nishabhosale0413@gmail.com>
2024-09-02 21:19:20 +0530

    assign 5

---
+++ Day5/program1.java
@@ -0,0 +1,14 @@
+class program1{
+	public static void main(String[] args){
+		int a=20; int b=30; int c=40;{
+			if(a>b && a>c){
+				System.out.println(a+" is largest among all numbers");
+			}else if(b>a && b>c){
+				System.out.println(b+" is largest among all numbers");
+			}else{
+				System.out.println(c+" is largest among all numbers");
+			}	
+
+		}
+	}
+}
---
+++ Day5/program2.java
@@ -0,0 +1,14 @@
+class program2{
+	public static void main(String[] args){
+		int a=20; int b=30; int c=40;{
+			if(a<b && a<c){
+				System.out.println(a+" is smallest among all numbers");
+			}else if(b<a && b<c){
+				System.out.println(b+" is smallest among all numbers");
+			}else{
+				System.out.println(c+" is smallest among all numbers");
+			}	
+
+		}
+	}
+}
