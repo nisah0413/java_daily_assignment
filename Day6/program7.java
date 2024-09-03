f14bd298f320e91c6c8543af11f761c150832305
nisha <nishabhosale0413@gmail.com>
2024-09-03 23:29:51 +0530

    assign 6

---
+++ Day6/program7.java
@@ -0,0 +1,13 @@
+class program7{
+	public static void main(String[]args){
+		char ch='A';
+		for(int i=1;i<=3;i++){
+			for(int j=1;j<=4;j++){
+			  System.out.print(" "+ch);
+			  ch++;
+			}
+	         System.out.println();
+		 ch='A';
+  		}
+	}
+}
